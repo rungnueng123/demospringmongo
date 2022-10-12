602165a17be384e149ad55ca109483b095f7b7ca
rungnueng123 <57011570@kmitl.ac.th>
2022-10-12 09:59:49 +0700

    first commit

---
+++ src/main/java/com/example/demospringmongo/DemospringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringmongoApplication.class, args);
+	}
+
+}
